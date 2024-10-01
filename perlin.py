967d71721a9afda646a15d2a9f23943bcb27e1f0
Kevin Chen <79341521+kevinddchen@users.noreply.github.com>
2024-09-30 22:55:01 -0700

    fix normalization

--- perlin.py
+++ perlin.py
@@ -32,8 +32,12 @@ def _hash_grid_point(x: int, y: int) -> tuple[float, float]:
     return math.cos(angle), math.sin(angle)
 
 
-def _compute_dot_product(grid_x: int, grid_y: int, x: float, y: float) -> float:
+def _grid_dot_product(grid_x: int, grid_y: int, x: float, y: float) -> float:
     """
+    Compute contribution to the Perlin noise corresponding to the dot product
+    between the random gradient vector at the grid point and the displacement
+    vector to the point.
+
     Args:
         grid_x: x coordinate of grid point.
         grid_y: y coordinate of grid point.
@@ -41,21 +45,24 @@ def _compute_dot_product(grid_x: int, grid_y: int, x: float, y: float) -> float:
         y: y coordinate of point.
 
     Returns:
-        Contribution to Perlin noise corresponding to the grid point.
+        Dot product value, normalized to [-1, 1].
     """
 
     grad_x, grad_y = _hash_grid_point(grid_x, grid_y)
     disp_x = x - grid_x
     disp_y = y - grid_y
-    return grad_x * disp_x + grad_y * disp_y
+    dot = grad_x * disp_x + grad_y * disp_y
+
+    # In 2d, dot is in [-1/sqrt(2), 1/sqrt(2)]. So normalize by multipling by sqrt(2).
+    return math.sqrt(2) * dot
 
 
 def _interpolate(a0: float, a1: float, t: float) -> float:
     """
-    Interpolate between a0 and a1, where t is in [0, 1].
+    Interpolate between a0 and a1, where t is in [0, 1]. Uses "smootherstep"
+    function, i.e. first and second derivatives vanish at endpoints.
     """
 
-    # use smootherstep
     return (a1 - a0) * t * t * t * (t * (6 * t - 15) + 10) + a0
 
 
@@ -67,10 +74,10 @@ def perlin(x: float, y: float) -> float:
     y0 = int(y)
 
     # compute contributions from each grid point
-    bottom_left = _compute_dot_product(x0, y0, x, y)
-    bottom_right = _compute_dot_product(x0 + 1, y0, x, y)
-    top_right = _compute_dot_product(x0 + 1, y0 + 1, x, y)
-    top_left = _compute_dot_product(x0, y0 + 1, x, y)
+    bottom_left = _grid_dot_product(x0, y0, x, y)
+    bottom_right = _grid_dot_product(x0 + 1, y0, x, y)
+    top_right = _grid_dot_product(x0 + 1, y0 + 1, x, y)
+    top_left = _grid_dot_product(x0, y0 + 1, x, y)
 
     # interpolation weights
     t_x = x - x0
